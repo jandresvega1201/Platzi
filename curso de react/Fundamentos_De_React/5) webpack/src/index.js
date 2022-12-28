d17a26581e7090dc3ce1a8ad6623e97e7cf73a7e
Andres vega <jandresvega@itsa.edu.co>
2022-12-27 19:18:38 -0500

    Agregar scrip de variables de entorno

--- curso de react/Fundamentos_De_React/5) webpack/src/index.js
+++ curso de react/Fundamentos_De_React/5) webpack/src/index.js
@@ -3,6 +3,6 @@ import '@styles/main.css';
 import '@styles/vars.styl';
 
 (async function App() {
-  const main = null || document.getElementById('main');
+  const main = document.getElementById('main');
   main.innerHTML = await Template();
 })();
