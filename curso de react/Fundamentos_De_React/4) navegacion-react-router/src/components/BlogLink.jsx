63eca93b960d9797ebaf98ed99ca594b1a7cc75a
Andres vega <jandresvega@itsa.edu.co>
2022-12-27 19:46:50 -0500

    #

--- curso de react/Fundamentos_De_React/4) navegacion-react-router/src/components/BlogLink.jsx
+++ curso de react/Fundamentos_De_React/4) navegacion-react-router/src/components/BlogLink.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 const BlogLink = ({post}) => {
+
     return (
         <li>
             <Link to={`/blog/${post.slug}`}>{post.title}</Link>
